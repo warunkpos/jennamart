bc626649581068cf22f59095a8c18697446f593d
warunkpos <hastapada02@gmail.com>
2024-09-26 22:15:11 +0700

    clear

--- script.js
+++ script.js
@@ -7,27 +7,69 @@ function setCookie(cname, cvalue) {
 
 
 ///list products------------------
-const products = [{
-    "id": 1,
-    "pluname": "Masako",
-    "price": 4500
-}, {
-    "id": 2,
-    "pluname": "Royko",
-    "price": 4500
-}, {
-    "id": 3,
-    "pluname": "Sabun Lervia",
-    "price": 3000
-}, {
-    "id": 4,
-    "pluname": "Sabun GIV",
-    "price": 2500
-}, {
-    "id": 5,
-    "pluname": "Sabun Harmony",
-    "price": 1500
-}]
+const products = [
+    {
+        "id": 1,
+        "pluname": "Masako",
+        "price": 4500
+    }, {
+        "id": 2,
+        "pluname": "Royko",
+        "price": 4500
+    }, {
+        "id": 3,
+        "pluname": "Sabun Lervia",
+        "price": 3000
+    }, {
+        "id": 4,
+        "pluname": "Sabun GIV",
+        "price": 2500
+    }, {
+        "id": 5,
+        "pluname": "Sabun Harmony",
+        "price": 1500
+    }, {
+        "id": 6,
+        "pluname": "Minyak Sunco 2l",
+        "price": 33000
+    }, {
+        "id": 7,
+        "pluname": "Minyak Sunco 1l",
+        "price": 16500
+    }, {
+        "id": 8,
+        "pluname": "Minyak Sania 1l",
+        "price": 16000
+    }, {
+        "id": 9,
+        "pluname": "Minyak Sovia 1l",
+        "price": 16000
+    }, {
+        "id": 10,
+        "pluname": "Minyak Brand 2l",
+        "price": 32000
+    }, {
+        "id": 11,
+        "pluname": "Gula Kemasan",
+        "price": 15500
+    }, {
+        "id": 12,
+        "pluname": "Gula Curah",
+        "price": 15000
+    }, {
+        "id": 13,
+        "pluname": "Detergen Bubuk",
+        "price": 4000
+    }, {
+        "id": 14,
+        "pluname": "Detergen Cair",
+        "price": 4500
+    }, {
+        "id": 15,
+        "pluname": "Detergen Bubuk Renceng",
+        "price": 4500
+    }
+]
 
 function appendListProducts(data) {
     for (var i = 0; i < data.length; i++) {
@@ -37,8 +79,8 @@ function appendListProducts(data) {
                 <img alt="Silhouette of mountains"
                     src="https://ionicframework.com/docs/img/demos/card-media.png" />
                 <ion-card-header>
+                    <ion-card-subtitle> Rp.`+ Number(data[i].price).toLocaleString('id-ID') + `</ion-card-subtitle>
                     <ion-card-title>`+ data[i].pluname + `</ion-card-title>
-                    <ion-card-subtitle>`+ data[i].price + `</ion-card-subtitle>
                 </ion-card-header> 
                 <ion-button expand="block" class="ion-btn-card ion-no-padding" onclick="addToCart(`+ data[i].id + `)">Add</ion-button>
             </ion-card>
@@ -69,7 +111,7 @@ function decrement() {
     }
 }
 
-const cartitem = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
+let cartitem = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
 setInterval(() => {
     document.getElementById('badgeCart').innerHTML = cartitem.length;
 }, 1000);
@@ -116,9 +158,16 @@ function closeCart() {
 }
 
 function printCart() {
-    const element = document.getElementById('printView');
     window.print();
 }
+
+function clearCart() {
+    cartitem = [];
+    localStorage.setItem('cart', []);
+    $('#listCart').html('');
+    $('#grandTotal').html('');
+}
+
 function openListCart() {
     $('#listCart').html('');
     $('#grandTotal').html('');
@@ -133,16 +182,16 @@ function appendListCart(data) {
         var dataHtml = `
             <ion-item-sliding>
                 <ion-item>
-                    <ion-label>`+ data[i].pluname + ` (Rp.`+ Number(data[i].price).toLocaleString('id-ID') + ` )</ion-label>
-                    <ion-badge color="primary" class="mr-20">`+ data[i].qty + `</ion-badge>
-                    <p> Rp. `+ Number(data[i].qty*data[i].price).toLocaleString('id-ID') + `</p>
+                    <ion-label>`+ data[i].pluname + ` (Rp.` + Number(data[i].price).toLocaleString('id-ID') + ` )</ion-label>
+                    <ion-badge class="mr-20 badge-qty">`+ data[i].qty + `</ion-badge>
+                    <p> Rp. `+ Number(data[i].qty * data[i].price).toLocaleString('id-ID') + `</p>
                 </ion-item>
                 <ion-item-options>
                     <ion-item-option color="danger"><ion-icon name="trash-outline"></ion-icon></ion-item-option>
                 </ion-item-options>
             </ion-item-sliding>`;
         $('#listCart').append(dataHtml);
-        total = total + (data[i].qty*data[i].price);
+        total = total + (data[i].qty * data[i].price);
     }
     if (total > 0) {
         var dataHtml = `
@@ -150,8 +199,8 @@ function appendListCart(data) {
                         <ion-label>Total</ion-label>
                         <p> Rp. `+ Number(total).toLocaleString('id-ID') + `</p>
                     </ion-item> `;
-            $('#grandTotal').append(dataHtml);
+        $('#grandTotal').append(dataHtml);
     }
     console.log(total);
-    
+
 }
